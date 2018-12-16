767b45a72f354dc3a562c6c4ec51c4371f17d69c
deinde dawodu <dopevilled@hotmail.com>
2018-12-16 10:18:20 -0500

    adding buffer and sound and window listener for mouse down to test

--- js/webaudio.js
+++ js/webaudio.js
@@ -1,13 +1,35 @@
 "use strict";
 
-const audiocontext = () => {
-  if (typeof audiocontext !== undefined) {
-    return audiocontext();
-  } else if (typeof webAudioContext !== undefined) {
-    return webAudioContext();
-  } else if (typeof mozAudioContext !== undefined) {
-    return mozAudioContext();
+const audioContextCheck = () => {
+  if (typeof AudioContext !== "undefined") {
+    return new AudioContext();
+  } else if (typeof webAudioContext !== "undefined") {
+    return new webAudioContext();
+  } else if (typeof mozAudioContext !== "undefined") {
+    return new mozAudioContext();
   } else {
     throw new Error("Audio Context is not supported Sucka!!!");
   }
 };
+
+const audiocontext = audioContextCheck();
+let audioBuffer = "";
+const getSound = new XMLHttpRequest();
+getSound.open("GET", "../sound/snare.WAV", "true");
+getSound.responseType = "arraybuffer";
+getSound.onload = () => {
+  audiocontext.decodeAudioData(getSound.response, buffer => {
+    audioBuffer = buffer;
+  });
+};
+
+getSound.send();
+
+const playBack = () => {
+  const playSound = audiocontext.createBufferSource();
+  playSound.buffer = audioBuffer;
+  playSound.connect(audiocontext.destination);
+  playSound.start(audiocontext.currentTime);
+};
+
+window.addEventListener("mousedown", playBack);
